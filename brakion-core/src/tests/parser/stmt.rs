70ecbd9f8fac4dac2ade1bca5a86644e9a8c9b10
Stanisław Zagórowski <hasohasacz@gmail.com>
2023-05-12 15:35:37 +0200

    if tests

--- brakion-core/src/tests/parser/stmt.rs
+++ brakion-core/src/tests/parser/stmt.rs
@@ -70,7 +70,7 @@ fn comment_with_newline_and_space() {
 }
 
 #[test]
-fn expr_stmt() {
+fn expr() {
     check_output_stmt(
         "1;",
         Some(Stmt {
@@ -84,7 +84,7 @@ fn expr_stmt() {
 }
 
 #[test]
-fn assignment_stmt() {
+fn assignment() {
     check_output_stmt(
         "a = 1;",
         Some(Stmt {
@@ -110,7 +110,7 @@ fn assignment_stmt() {
 }
 
 #[test]
-fn variable_stmt() {
+fn variable() {
     check_output_stmt(
         "var a = 1;",
         Some(Stmt {
@@ -165,6 +165,171 @@ fn return_stmt() {
                 span: test_span(1, 7),
             }),
             span: test_span(1, 8),
+        }),
+    );
+}
+
+#[test]
+fn empty_block() {
+    check_output_stmt(
+        "{}",
+        Some(Stmt {
+            kind: StmtKind::Block(vec![]),
+            span: test_span(1, 3),
+        }),
+    );
+}
+
+#[test]
+fn block_with_stmt() {
+    check_output_stmt(
+        "{ 1; }",
+        Some(Stmt {
+            kind: StmtKind::Block(vec![Stmt {
+                kind: StmtKind::Expr(Expr {
+                    kind: ExprKind::Literal(Literal::Int(1)),
+                    span: test_span(3, 4),
+                }),
+                span: test_span(3, 5),
+            }]),
+            span: test_span(1, 7),
+        }),
+    );
+}
+
+#[test]
+fn block_with_stmts() {
+    check_output_stmt(
+        "{ 1; 2; }",
+        Some(Stmt {
+            kind: StmtKind::Block(vec![
+                Stmt {
+                    kind: StmtKind::Expr(Expr {
+                        kind: ExprKind::Literal(Literal::Int(1)),
+                        span: test_span(3, 4),
+                    }),
+                    span: test_span(3, 5),
+                },
+                Stmt {
+                    kind: StmtKind::Expr(Expr {
+                        kind: ExprKind::Literal(Literal::Int(2)),
+                        span: test_span(6, 7),
+                    }),
+                    span: test_span(6, 8),
+                },
+            ]),
+            span: test_span(1, 10),
+        }),
+    );
+}
+
+#[test]
+fn if_stmt() {
+    check_output_stmt(
+        "if true { 2; }",
+        Some(Stmt {
+            kind: StmtKind::If {
+                condition: Expr {
+                    kind: ExprKind::Literal(Literal::Bool(true)),
+                    span: test_span(4, 8),
+                },
+                then: Box::new(Stmt {
+                    kind: StmtKind::Block(vec![Stmt {
+                        kind: StmtKind::Expr(Expr {
+                            kind: ExprKind::Literal(Literal::Int(2)),
+                            span: test_span(11, 12),
+                        }),
+                        span: test_span(11, 13),
+                    }]),
+                    span: test_span(9, 15),
+                }),
+                otherwise: None,
+            },
+            span: test_span(1, 15),
+        }),
+    );
+}
+
+#[test]
+fn if_else() {
+    check_output_stmt(
+        "if true { 2; } else { 3; }",
+        Some(Stmt {
+            kind: StmtKind::If {
+                condition: Expr {
+                    kind: ExprKind::Literal(Literal::Bool(true)),
+                    span: test_span(4, 8),
+                },
+                then: Box::new(Stmt {
+                    kind: StmtKind::Block(vec![Stmt {
+                        kind: StmtKind::Expr(Expr {
+                            kind: ExprKind::Literal(Literal::Int(2)),
+                            span: test_span(11, 12),
+                        }),
+                        span: test_span(11, 13),
+                    }]),
+                    span: test_span(9, 15),
+                }),
+                otherwise: Some(Box::new(Stmt {
+                    kind: StmtKind::Block(vec![
+                        Stmt {
+                            kind: StmtKind::Expr(Expr {
+                                kind: ExprKind::Literal(Literal::Int(3)),
+                                span: test_span(23, 24),
+                            }),
+                            span: test_span(23, 25),
+                        },
+                    ]),
+                    span: test_span(21, 27),
+                })),
+            },
+            span: test_span(1, 27),
+        }),
+    );
+}
+
+#[test]
+fn if_else_chain() {
+    check_output_stmt(
+        "if false 1; else if true 2; else 3;",
+        Some(Stmt {
+            kind: StmtKind::If {
+                condition: Expr {
+                    kind: ExprKind::Literal(Literal::Bool(false)),
+                    span: test_span(4, 9),
+                },
+                then: Box::new(Stmt {
+                    kind: StmtKind::Expr(Expr {
+                        kind: ExprKind::Literal(Literal::Int(1)),
+                        span: test_span(10, 11),
+                    }),
+                    span: test_span(10, 12),
+                }),
+                otherwise: Some(Box::new(Stmt {
+                    kind: StmtKind::If {
+                        condition: Expr {
+                            kind: ExprKind::Literal(Literal::Bool(true)),
+                            span: test_span(21, 25),
+                        },
+                        then: Box::new(Stmt {
+                            kind: StmtKind::Expr(Expr {
+                                kind: ExprKind::Literal(Literal::Int(2)),
+                                span: test_span(26, 27),
+                            }),
+                            span: test_span(26, 28),
+                        }),
+                        otherwise: Some(Box::new(Stmt {
+                            kind: StmtKind::Expr(Expr {
+                                kind: ExprKind::Literal(Literal::Int(3)),
+                                span: test_span(34, 35),
+                            }),
+                            span: test_span(34, 36),
+                        })),
+                    },
+                    span: test_span(18, 36),
+                })),
+            },
+            span: test_span(1, 36),
         }),
     );
 }
